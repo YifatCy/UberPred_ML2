40c07512fe2299d52c6cb7e5e48d854b59da09d2
YifatCy <77786906+YifatCy@users.noreply.github.com>
2021-08-26 14:49:34 +0300

    commit

---
+++ auxiliary_functions.py
@@ -0,0 +1,125 @@
+import numpy as np
+import itertools
+
+BEGIN = '*B'
+STOP = '*S'
+CONTAINS_DIGIT = '*CD'
+CONTAINS_UPPER = '*CU'
+CONTAINS_HYPHEN = '*CH'
+
+
+
+def get_x_any_y(df, dates, y_column):
+    x, y = [], []
+    for date in dates:
+        day_df = df[df['date'] == date]
+        x.append(day_df.drop([y_column, 'date', 'year'], axis=1).to_numpy())
+        y.append(day_df[y_column].to_numpy())
+        print(x,y)
+    return x, y
+
+
+def get_x_any_y_years(df, years, y_column):
+    x, y = [], []
+    for year in years:
+        years_df = df[df['year'] == year]
+        x.append(years_df.drop([y_column, 'date', 'year'], axis=1).to_numpy())
+        y.append(years_df[y_column].to_numpy())
+    return x, y
+
+
+def index_to_value(x, num_of_hours):
+
+    values = []
+
+    while x > 0:
+        values.append(x % 3)
+        x //= 3
+
+    while len(values) < num_of_hours:
+        values.append(0)
+
+    values.reverse()
+
+    return np.array(values)
+
+
+def value_to_index(lst):
+
+    res = 0
+    for index, value in enumerate(reversed(lst)):
+        res += value * 3 ** index
+
+    return res
+
+
+def get_x_any_y_advanced_creative(df, weeks, y_column, k):
+
+    x, y = {}, {}
+
+    assert 24 % k == 0
+
+    for week_index, week in enumerate(weeks):
+
+        x[week_index] = []
+        y[week_index] = []
+
+        x_week = {i: [] for i in range(24//k)}
+        y_week = {i: [] for i in range(24//k)}
+
+        week_df = df[df['year_week'] == week]
+        for week_day in sorted(week_df['week_day'].unique()):
+
+            x_day_array = week_df[week_df['week_day'] == week_day].drop([y_column, 'year_week'], axis=1).to_numpy()
+
+            y_hours_labels = {week_df[week_df['week_day'] == week_day]['hour'].iloc[i]: week_df[week_df['week_day'] == week_day][y_column].iloc[0] for i in range(len(week_df[week_df['week_day'] == week_day].index))}
+            x_hours_vectors = {week_df[week_df['week_day'] == week_day]['hour'].iloc[i]: week_df[week_df['week_day'] == week_day].drop([y_column, 'year_week'], axis=1).iloc[0].to_numpy() for i in range(len(week_df[week_df['week_day'] == week_day].index))}
+
+
+            hour_index = list(week_df[week_df['week_day'] == week_day].drop([y_column, 'year_week'], axis=1).columns).index('hour')
+
+            # No missing hours:
+            if len(x_day_array) == 24:
+                for block_index in range(24 // k):
+                    hours_vectors = [x_hours_vectors[hour] for hour in range(k * block_index, k * (block_index + 1), 1)]
+                    x_week[block_index].append(list(itertools.chain(*hours_vectors)))
+
+                    y_week[block_index].append(value_to_index([y_hours_labels[hour] for hour in range(k * block_index, k * (block_index + 1), 1)]))
+
+            # missing hours. should discard windows with missing hours.
+            else:
+                missing_hours = [hour for hour in range(24) if hour not in x_day_array[:, hour_index]]
+                missing_blocks = set()
+
+                for block_index in range(24 // k):
+                    for block_hour in range(k * block_index, k * (block_index + 1), 1):
+                        if block_hour in missing_hours:
+                            missing_blocks.add(block_index)
+
+                for block_index in range(24 // k):
+                    if block_index not in missing_blocks:
+
+                        hours_vectors = [x_hours_vectors[hour] for hour in range(k * block_index, k * (block_index + 1), 1)]
+                        x_week[block_index].append(list(itertools.chain(*hours_vectors)))
+
+                        y_week[block_index].append(value_to_index([
+                            y_hours_labels[hour] for hour in range(k * block_index, k * (block_index + 1), 1)]))
+
+
+        for block_index in range(24 // k):
+            x[week_index].append(x_week[block_index])
+            y[week_index].append(np.array(y_week[block_index]))
+
+
+    return x, y
+
+
+
+def create_month_dict(data):
+    months = []
+    for x in data:
+        month = x[0, -2]
+        if month not in months:
+            months.append(month)
+    month_dict = {val: i for i, val in enumerate(sorted(months))}
+    return month_dict
---
+++ preprocssing.py
@@ -0,0 +1,141 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+#from auxiliary_functions import get_x_any_y_advanced_creative, get_x_any_y, get_x_any_y_years
+import datetime
+
+X_COLUMNS = ['year', 'month', 'day', 'hour']
+Y_COLUMN = 'loadrank'
+
+TEST_SIZE = 0.3
+
+
+def get_x_any_y(df, dates, y_column):
+    x, y = [], []
+    for date in dates:
+        day_df = df[df['date'] == date]
+        x.append(day_df.drop([y_column, 'date', 'year'], axis=1).to_numpy())
+        y.append(day_df[y_column].to_numpy())
+    return x, y
+
+
+def prepare():
+    apr = pd.read_csv("datasets/uber-raw-data-apr14.csv")
+    may = pd.read_csv("datasets/uber-raw-data-may14.csv")
+    jun = pd.read_csv("datasets/uber-raw-data-jun14.csv")
+    jul = pd.read_csv("datasets/uber-raw-data-jul14.csv")
+    aug = pd.read_csv("datasets/uber-raw-data-aug14.csv")
+    sep = pd.read_csv("datasets/uber-raw-data-sep14.csv")
+
+    data = [apr,may,jun,jul,aug,sep]
+    data = pd.concat(data)
+
+    mm = []
+    dd = []
+    yy = []
+    hrs = []
+    dow = []
+    dow_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    dt_time = data['Date/Time'].copy(deep=True).to_numpy()
+    for i in range(len(dt_time)):
+        mm.append(dt_time[i].split(" ")[0].split("/")[0])
+
+    dt_time = data['Date/Time'].copy(deep=True).to_numpy()
+    for i in range(len(dt_time)):
+        dd.append(dt_time[i].split(" ")[0].split("/")[1])
+
+    dt_time = data['Date/Time'].copy(deep=True).to_numpy()
+    for i in range(len(dt_time)):
+        yy.append(dt_time[i].split(" ")[0].split("/")[2])
+
+    dt_time = data['Date/Time'].copy(deep=True).to_numpy()
+    for i in range(len(dt_time)):
+        hrs.append(dt_time[i].split(" ")[1].split(":")[0])
+
+    for i in range(len(mm)):
+        dow.append(dow_list[datetime.date(int(yy[i]),
+                                          int(mm[i]),
+                                          int(dd[i])).weekday()])
+
+
+    data['month'] = mm
+    data['day'] = dd
+    data['year'] = yy
+    data['hour'] = hrs
+    data['day literal'] = dow
+    data['date'] = data['day'] + data['month'] + data['year']
+    del data['Date/Time']
+    data.to_csv("datasets/uber.csv")
+
+def prepare_dataset():
+    #prepare()
+    df = pd.read_csv("datasets/uber.csv")
+    df = df[['year','month','day','date','hour']]
+    df_agg = df.groupby(['year','month','day','date','hour']).size().reset_index(name='counts')
+    return df_agg
+
+
+def prepare_categorized_dataset():
+    prepare()
+    df = pd.read_csv("datasets/uber.csv")
+    df = df[['year', 'month', 'day', 'date', 'hour']]
+    df_agg = df.groupby(['year', 'month', 'day', 'date', 'hour']).size().reset_index(name='counts')
+    max_count = max(df_agg['counts'])
+    # print(max_count)
+    max_range = range(max_count)
+    quantile = np.quantile(max_range, q=[0.25, 0.5, 0.75])
+    df_agg['loadrank'] = df_agg["counts"].apply(
+        lambda x: 0 if x <= quantile[0] else (1 if x <= quantile[1] else (2 if x <= quantile[2] else (3))))
+    # print(df_agg)
+    df_agg.to_csv("datasets/uber_hour_categorized.csv")
+    #print(df_agg)
+    return df_agg
+#prepare()
+#base_agg()
+
+
+def prepare_train_test(categorized=False, scale=True, **kwargs):
+    seed = kwargs.get("seed", 57)
+
+    df = prepare_categorized_dataset()
+
+    X = df.drop([Y_COLUMN, 'date','year'], axis=1)
+    Y = df[Y_COLUMN]
+
+    train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size = TEST_SIZE, random_state = seed)
+
+    if scale:
+        scalar = StandardScaler()
+        scalar.fit(train_x)
+
+        scaled_train_x = scalar.transform(train_x)
+        scaled_test_x = scalar.transform(test_x)
+        return scaled_test_x, test_y, scaled_train_x, train_y
+
+    return test_x, test_y, train_x, train_y
+
+
+def prepare_grouped_data(categorized=False, scale=True):
+    df = prepare_categorized_dataset()
+    dates_in_data = np.unique(df['date'])
+    train_days, test_days = train_test_split(dates_in_data,
+                                             test_size=TEST_SIZE,
+                                             random_state=57)
+
+    train_x, train_y = get_x_any_y(df, train_days, Y_COLUMN)
+    test_x, test_y = get_x_any_y(df, test_days, Y_COLUMN)
+
+    if scale:
+        train_set = df[df['date'].isin(train_days)]
+        train_set_x = train_set.drop([Y_COLUMN, 'date','year'], axis=1)
+        scalar = StandardScaler()
+        scalar.fit(train_set_x)
+
+        scaled_train_x = [scalar.transform(day) for day in train_x]
+        scaled_test_x = [scalar.transform(day) for day in test_x]
+        return scaled_train_x, train_y, scaled_test_x, test_y
+
+    return train_x, train_y, test_x, test_y
