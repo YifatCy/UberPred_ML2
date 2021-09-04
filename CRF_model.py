20e5b861c7b245ca43b9bd43ccd0ca0beeb0da0c
YifatCy <77786906+YifatCy@users.noreply.github.com>
2021-09-04 13:34:04 +0300

    commit

---
+++ CRF_model.py
@@ -0,0 +1,62 @@
+import pycrfsuite
+import numpy as np
+from preprocssing import prepare_grouped_data
+
+X_COLUMNS = ['hour', 'month','day', 'day_literal', 'spd', 'vsb','temp', 'dewp', 'slp', 'sd', 'hday']
+Y_COLUMN = 'loadrank'
+
+
+def train_and_save_model(X_train, y_train, model_path='crf_model.crfsuite'):
+    X_train = [date2features(x) for x in X_train]
+    y_train = [[str(label) for label in y] for y in y_train]
+    trainer = pycrfsuite.Trainer(verbose=False)
+    for xseq, yseq in zip(X_train, y_train):
+        trainer.append(xseq, yseq)
+    trainer.set_params({
+        'c1': 1.0,  # coefficient for L1 penalty
+        'c2': 1e-3,  # coefficient for L2 penalty
+        'max_iterations': 150,  # stop earlier
+        # include transitions that are possible, but not observed
+        'feature.possible_transitions': True
+    })
+    trainer.train(model_path)
+
+
+def evaluate_model(X_test, y_test, model_path='crf_model.crfsuite'):
+    X_test = [date2features(x) for x in X_test]
+    y_test = [[str(label) for label in y] for y in y_test]
+    tagger = pycrfsuite.Tagger()
+    tagger.open(model_path)
+    acc = 0
+    for date, labels in zip(X_test, y_test):
+        predicted = np.array(tagger.tag(date))
+        corrected = np.array(labels)
+        acc += np.sum(predicted == corrected) / (24 * len(X_test))
+    print(f'Accuracy: {acc}')
+
+
+def hour2features(hours, i):
+    curr_hour = hours[i]
+    features = [column + '=' + str(value) for column, value in zip(X_COLUMNS, curr_hour)]
+    features.append('bias')
+    if i > 0:
+        prev_hour = hours[i-1]
+        prev_features = ['prev_'+column + '=' + str(value) for column, value in zip(X_COLUMNS, prev_hour)]
+        features.extend(prev_features)
+    else:
+        features.append('BOS')
+
+    return features
+
+
+def date2features(day):
+    return [hour2features(day, i) for i in range(len(day))]
+
+
+if __name__ == '__main__':
+    # main
+    model_location = 'crf_model.crfsuite'
+    X_train, y_train, X_test, y_test = prepare_grouped_data(scale=False)
+    train_and_save_model(X_train, y_train, model_path=model_location)
+    evaluate_model(X_test, y_test, model_path=model_location)
+
