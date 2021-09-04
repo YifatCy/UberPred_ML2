036137c2b4a5acfb02d3805b47bdeb4c3d35c294
YifatCy <77786906+YifatCy@users.noreply.github.com>
2021-09-04 23:43:13 +0300

    commit

--- preprocssing.py
+++ preprocssing.py
@@ -21,7 +21,7 @@ def get_x_any_y(df, dates, y_column):
     x, y = [], []
     for date in dates:
         day_df = df[df['date'] == date]
-        x.append(day_df.drop([y_column,'pickup_dt','pickups','date','year'], axis=1).to_numpy())
+        x.append(day_df.drop([y_column,'pickups','date','year'], axis=1).to_numpy())
         y.append(day_df[y_column].to_numpy())
     return x, y
 
@@ -30,7 +30,7 @@ def get_x_any_y_years(df, years, y_column):
     x, y = [], []
     for year in years:
         years_df = df[df['year'] == year]
-        x.append(years_df.drop([y_column, 'pickup_dt','date', 'year'], axis=1).to_numpy())
+        x.append(years_df.drop([y_column,'date', 'year'], axis=1).to_numpy())
         y.append(years_df[y_column].to_numpy())
     return x, y
 
@@ -72,7 +72,7 @@ def prepare():
     data['hour'] = hrs
     data['day_literal'] = dow
     data['date'] = data['year'] + data['month'] + data['day']
-    #del data['pickup_dt']  ## TODO check if it affects the others
+    del data['pickup_dt']  ## TODO check if it affects the others
     del data['pcp01']
     del data['pcp06']
     del data['pcp24']
@@ -95,7 +95,7 @@ def group_by_borough(df):
     #df1 = df.groupby(['date', 'hour', 'month', 'day', 'day_literal', 'year', 'spd', 'vsb', 'temp', 'dewp', 'slp', 'sd', 'hday'], observed=True)['pickups'].sum().reset_index()
     #df1.to_csv("1.csv")
     # TODO check if it affects the others
-    df1 = df.groupby(['pickup_dt','date', 'hour', 'month', 'day', 'day_literal', 'year', 'spd', 'vsb', 'temp', 'dewp', 'slp', 'sd', 'hday'], observed=True)['pickups'].sum().reset_index()
+    df1 = df.groupby(['date', 'hour', 'month', 'day', 'day_literal', 'year', 'spd', 'vsb', 'temp', 'dewp', 'slp', 'sd', 'hday'], observed=True)['pickups'].sum().reset_index()
     #df2.drop(['pickup_dt'],axis=1).to_csv("2.csv")
     return df1
 
@@ -120,7 +120,7 @@ def prepare_train_test(categorized=False, scale=True, **kwargs):
     seed = kwargs.get("seed", 57)
 
     df = prepare_categorized_dataset()
-    X = df.drop([Y_COLUMN,"pickup_dt","pickups", 'date','year'], axis=1)
+    X = df.drop([Y_COLUMN,"pickups", 'date','year'], axis=1)
     Y = df[Y_COLUMN]
 
     train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size = TEST_SIZE, random_state = seed)
@@ -147,7 +147,7 @@ def prepare_grouped_data(categorized=False, scale=True):
     test_x, test_y = get_x_any_y(df, test_days, Y_COLUMN)
     if scale:
         train_set = df[df['date'].isin(train_days)]
-        train_set_x = train_set.drop([Y_COLUMN, "pickup_dt","pickups", 'date','year'], axis=1)
+        train_set_x = train_set.drop([Y_COLUMN, "pickups", 'date','year'], axis=1)
         scalar = StandardScaler()
         scalar.fit(train_set_x)
         print(train_set_x)
@@ -175,7 +175,6 @@ def prepare_dataset_advanced():
     df.drop(['pickup_dt', 'date'], axis=1, inplace=True)
     return df
 
-prepare_dataset_advanced()
 
 def prepare_grouped_data_advanced(num_of_hours):
 
@@ -203,7 +202,7 @@ def divide_data_to_two_years(scale=True):
 
     if scale:
         train_set = df[df['year'].isin(train_years)]
-        train_set_x = train_set.drop([Y_COLUMN,'pickup_dt', 'date', 'year'], axis=1)
+        train_set_x = train_set.drop([Y_COLUMN, 'date', 'year'], axis=1)
 
         scalar = StandardScaler()
         scalar.fit(train_set_x)
@@ -283,3 +282,127 @@ def get_x_any_y_advanced_creative(df, weeks, y_column, k):
 
 
     return x, y
+
+
+def over_sampling():
+    ## over sampling
+    data = prepare_categorized_dataset()
+    num_0 = len(data[data[Y_COLUMN] == 0])
+    num_1 = len(data[data[Y_COLUMN] == 1])
+    num_2 = len(data[data[Y_COLUMN] == 2])
+    num_3 = len(data[data[Y_COLUMN] == 3])
+    #ratio = num_1 / num_0
+    new_data = pd.DataFrame(columns=data.columns)
+    cnt = 0
+    for idx,point in data.iterrows():
+        # reshaped_point = point.reshape(1, -1)
+        if point[Y_COLUMN] == 0 or point[Y_COLUMN] == 1:
+            new_data.loc[-1] = point
+            new_data.index = new_data.index + 1
+            continue
+        p = np.random.rand()
+        if p > 0.5:
+            cnt += 1
+            new_data.loc[-1] = point
+            new_data.index = new_data.index + 1
+            new_data.loc[-1] = point
+            new_data.index = new_data.index + 1
+        else:
+            new_data.loc[-1] = point
+            new_data.index = new_data.index + 1
+    print('cnt ', cnt)
+    print('over sampling ',len(new_data))
+    #new_data = shuffle(new_data)
+    for i in ['date','hour', 'month','day', 'day_literal', 'year','spd', 'temp', 'dewp', 'hday','pickups','loadrank']:
+        new_data[i] = new_data[i].astype(int)
+    new_data.to_csv('over_sampling.csv')
+    print('over_sampling',new_data)
+    df = new_data.groupby('loadrank')['pickups'].count().reset_index(name='sum_uber_pickups')
+    df = df.set_index('loadrank')
+    df.plot.bar(rot=0, title=f"Hist of Hours per Category [0, 1, 2, 3]", alpha=0.7, color='salmon')
+    plt.ylabel('Sum Hours')
+    plt.xlabel('Category')
+    #plt.savefig('figures/hist_per_each_category.png')
+    plt.show()
+    return new_data
+
+
+def under_sampling():
+    ## under sampling
+    data = prepare_categorized_dataset()
+    # num_0 = len(data[data['song_popularity'] == 0])
+    # num_1 = len(data[data['song_popularity'] == 1])
+    # ratio = num_1 / num_0
+    new_data = pd.DataFrame(columns=data.columns)
+    cnt = 0
+    for idx,point in data.iterrows():
+        # reshaped_point = point.reshape(1, -1)
+        if point[Y_COLUMN] == 3 or point[Y_COLUMN] == 2:
+            new_data.loc[-1] = point
+            new_data.index = new_data.index + 1
+            continue
+        p = np.random.rand()
+        if p >= 0.5:
+            cnt += 1
+            new_data.loc[-1] = point
+            new_data.index = new_data.index + 1
+    print('cnt ', cnt)
+    print('under sampling ', len(new_data))
+    #new_data = shuffle(new_data)
+    for i in ['date','hour', 'month','day', 'day_literal', 'year','spd', 'temp', 'dewp', 'hday','pickups','loadrank']:
+        new_data[i] = new_data[i].astype(int)
+    print('under_sampling',new_data)
+    new_data.to_csv('under_sampling.csv')
+    print('over_sampling',new_data)
+    df = new_data.groupby('loadrank')['pickups'].count().reset_index(name='sum_uber_pickups')
+    df = df.set_index('loadrank')
+    df.plot.bar(rot=0, title=f"Hist of Hours per Category [0, 1, 2, 3]", alpha=0.7, color='salmon')
+    plt.ylabel('Sum Hours')
+    plt.xlabel('Category')
+    #plt.savefig('figures/hist_per_each_category.png')
+    plt.show()
+    return new_data
+
+def prepare_grouped_data_over(categorized=False, scale=True):
+    over_sampling()
+    df = pd.read_csv("over_sampling.csv", index_col=False)
+    dates_in_data = np.unique(df['date'])
+    train_days, test_days = train_test_split(dates_in_data,
+                                             test_size=TEST_SIZE,
+                                             random_state=57)
+
+    train_x, train_y = get_x_any_y(df, train_days, Y_COLUMN)
+    test_x, test_y = get_x_any_y(df, test_days, Y_COLUMN)
+    if scale:
+        train_set = df[df['date'].isin(train_days)]
+        train_set_x = train_set.drop([Y_COLUMN, "pickups", 'date','year'], axis=1)
+        scalar = StandardScaler()
+        scalar.fit(train_set_x)
+
+        scaled_train_x = [scalar.transform(day) for day in train_x]
+        scaled_test_x = [scalar.transform(day) for day in test_x]
+        return scaled_train_x, train_y, scaled_test_x, test_y
+
+    return train_x, train_y, test_x, test_y
+
+def prepare_grouped_data_under(categorized=False, scale=True):
+    under_sampling()
+    df = pd.read_csv("under_sampling.csv", index_col=False)
+    dates_in_data = np.unique(df['date'])
+    train_days, test_days = train_test_split(dates_in_data,
+                                             test_size=TEST_SIZE,
+                                             random_state=57)
+
+    train_x, train_y = get_x_any_y(df, train_days, Y_COLUMN)
+    test_x, test_y = get_x_any_y(df, test_days, Y_COLUMN)
+    if scale:
+        train_set = df[df['date'].isin(train_days)]
+        train_set_x = train_set.drop([Y_COLUMN,"pickups", 'date','year'], axis=1)
+        scalar = StandardScaler()
+        scalar.fit(train_set_x)
+
+        scaled_train_x = [scalar.transform(day) for day in train_x]
+        scaled_test_x = [scalar.transform(day) for day in test_x]
+        return scaled_train_x, train_y, scaled_test_x, test_y
+
+    return train_x, train_y, test_x, test_y
